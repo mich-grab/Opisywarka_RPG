91b76e3ab7ab4bf84aacb3c7b212b75e5ef2f449
migrabo <103949950+migrabo@users.noreply.github.com>
2022-05-22 20:29:12 +0000

    Update script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+const Btn = document.getElementById('#Btn')
+/*
+const obj = JSON.parse(fetch("opisywarka.json")
+    .then(response => response.json())
+)
+
+function Random(number) {
+    const random_number = Math.floor(Math.random() * number)
+    return random_number
+}
+
+Btn.addEventListener('click', () => {
+    document.getElementById('dzwiek_wynik').innerText = data.Dzwiek[Random(data.Dzwiek.length)]
+    document.getElementById('widok_wynik').innerText = data.Widok[Random(data.Widok.length)]
+    document.getElementById('dotyk_wynik').innerText = data.Dotyk[Random(data.Dotyk.length)]
+    document.getElementById('zapach_wynik').innerText = data.Zapach[Random(data.Zapach.length)]
+    document.getElementById('odczucia_wynik').innerText = data.Odczucia[Random(data.Odczucia.length)]
+}) */
+
+const myRequest = new Request('opisywarka.json');
+
+
+
+fetch(myRequest)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('dzwiek_wynik').innerText = data.Dzwiek[Random(data.Dzwiek.length)]
+        document.getElementById('widok_wynik').innerText = data.Widok[Random(data.Widok.length)]
+        document.getElementById('dotyk_wynik').innerText = data.Dotyk[Random(data.Dotyk.length)]
+        document.getElementById('zapach_wynik').innerText = data.Zapach[Random(data.Zapach.length)]
+        document.getElementById('odczucia_wynik').innerText = data.Odczucia[Random(data.Odczucia.length)]
+    })
+    .catch(console.error);
+
+
+
+function Random(number) {
+    const random_number = Math.floor(Math.random() * number)
+    return random_number
+}
